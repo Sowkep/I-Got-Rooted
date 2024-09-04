d4458b00f450565c46032205e1bc06332049c693
sowkep <undefiend@undefiend.undefiend>
2024-09-04 23:34:46 +0900

    commit

--- src/mods/mods_menu.h
+++ src/mods/mods_menu.h
@@ -10,26 +10,20 @@
 inline int mods_menu(){
     DIR *directory;
     directory = opendir(DEFAULT_MODS_FOLDER);
-<<<<<<< HEAD
     struct dirent *entry;
-=======
->>>>>>> a4cd446ed9d432f3d07722efb3e453610e0ffcac
+
     if(directory == NULL){
         puts("Unable to open directory");
         return 1;
     }
     else{
-<<<<<<< HEAD
         while( (entry = readdir(directory)) ){
             printf("File: %s\n", entry->d_name);
             //neeed to include entry->d_name        
         }
-=======
-        puts("Opened");
-        printf("%s\n", readdir(directory)->d_name);
->>>>>>> a4cd446ed9d432f3d07722efb3e453610e0ffcac
     }
     closedir(directory);
+    
     return 0;
 }
 

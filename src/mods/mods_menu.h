a4cd446ed9d432f3d07722efb3e453610e0ffcac
kybex11 <artm.22@internet.ru>
2024-09-04 17:23:45 +0000

    commit

--- src/mods/mods_menu.h
+++ src/mods/mods_menu.h
@@ -1,21 +1,21 @@
 #ifndef MODS_MENU_H
 #define MODS_MENU_H
 
-#define DEFAULT_MODS_FOLDER "default/"
-#define DEFAULT_USERS_FOLDER "users/"
+#define DEFAULT_MODS_FOLDER "src/mods/default/"
+#define DEFAULT_USERS_FOLDER "src/mods/users/"
 
 #include <stdio.h>
 #include <dirent.h>
 
 int mods_menu(){
     DIR *directory;
-    directory = opendir(".");
+    directory = opendir(DEFAULT_MODS_FOLDER);
     if(directory == NULL){
-        puts("unable to open directory");
+        puts("Unable to open directory");
         return 1;
     }
     else{
-        puts("opened");
+        puts("Opened");
         printf("%s\n", readdir(directory)->d_name);
     }
     closedir(directory);
